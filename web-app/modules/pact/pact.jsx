02953255c7f1adf9932c53bbc10099209cafbb01
Arthur <thearthurchen@gmail.com>
2021-11-26 21:56:39 -0800

    Update chart w/ EIA

--- web-app/modules/pact/pact.jsx
+++ web-app/modules/pact/pact.jsx
@@ -5,7 +5,10 @@ import {
   grommet,
   Grommet,
   ResponsiveContext,
-  Heading, DataChart, Text, Spinner,
+  Heading,
+  DataChart,
+  Text,
+  Spinner,
 } from "grommet";
 import { ethers } from "ethers";
 import { Contract } from "@ethersproject/contracts";
@@ -13,8 +16,7 @@ import { ProviderContext } from "../hooks";
 import SignerContext from "../hooks/useSigner";
 import { abis } from "../contracts";
 import PactContext from "../hooks/usePact";
-import moment from 'moment';
-
+import moment from "moment";
 
 const data = [];
 for (let i = 0; i < 13; i += 1) {
@@ -26,24 +28,22 @@ for (let i = 0; i < 13; i += 1) {
 }
 
 const formatData = (datapoints) => {
-  return datapoints.map( point => {
+  return datapoints.map((point) => {
     return {
       date: moment.format(),
-      amount: point
-    }
-  })
-}
-
+      amount: point,
+    };
+  });
+};
 
 export const PACT_STATES = {
   0: "Disabled",
   1: "Idle",
   2: "Running",
-  3: "Complete"
+  3: "Complete",
 };
 
 const Pact = ({ address }) => {
-
   address = "whatever test for now";
 
   // Web3
@@ -52,24 +52,50 @@ const Pact = ({ address }) => {
   const { pact, setPact } = useContext(PactContext);
 
   // Helpers for UX/UI
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   // Contract Information
   const [eiaRegion, setEiaRegion] = useState();
-  const [listingState, setListingState] = useState(null);
+  const [pactState, setPactState] = useState(null);
   const [nestData, setNestData] = useState([]);
   const [complianceData, setComplianceData] = useState([]);
+  const [gridLoad, setGridLoad] = useState([]);
+
+  const getEIAData = async () => {
+    const response = await fetch(
+      "https://www.eia.gov/electricity/930-api/region_data/series_data?type[0]=TI&respondent[0]=LDWP&start=11192021 08:00:00&end=11272021 08:00:00&frequency=hourly&timezone=Pacific&limit=10000&offset=0"
+    );
+    const jsonData = await response.json();
+    const data = jsonData[0];
+    console.log(data.data);
+    const values = data.data[0]["VALUES"];
+    console.log(values);
+    const data_values = values["DATA"];
+    const date_values = values["DATES"];
+    let data_points;
+    if (data_values.length !== date_values.length) {
+      console.log(`Reported data and dates doesn't match in length`);
+      return;
+    }
+    data_points = data_values.map((data_point, idx) => {
+      return {
+        amount: -data_point,
+        date: date_values[idx],
+      };
+    });
+    console.log(data_points);
+    setGridLoad(data_points);
+  };
+  useEffect(() => {
+    getEIAData();
+  }, []);
 
   useEffect(() => {
     if (address === null || provider === null || signer === null) {
       return;
     }
     const contractAddress = ethers.utils.getAddress(address);
-    const newPact = new Contract(
-      contractAddress,
-      abis.Pact.abi,
-      provider
-    );
+    const newPact = new Contract(contractAddress, abis.Pact.abi, provider);
     setPact(newPact);
   }, [address, provider, signer]);
 
@@ -80,18 +106,18 @@ const Pact = ({ address }) => {
 
     async function setData() {
       setLoading(true);
-      const [
-        _eiaRegion,
-        _signerAddress,
-        _pactState
-      ] = await Promise.all([
+      const [_eiaRegion, _signerAddress, _pactState] = await Promise.all([
         await pact.connect(signer).EIARegion(),
         await signer.getAddress(),
-        await pact.connect(signer).PactState()
+        await pact.connect(signer).PactState(),
       ]);
       setLoading(false);
-      const _nestData = await pact.connect(signer).userAddressToNestData(_signerAddress);
-      const _complianceData = await pact.connect(signer).userAddressToComplianceData(_signerAddress);
+      const _nestData = await pact
+        .connect(signer)
+        .userAddressToNestData(_signerAddress);
+      const _complianceData = await pact
+        .connect(signer)
+        .userAddressToComplianceData(_signerAddress);
 
       setEiaRegion(_eiaRegion);
       // TODO parse out the nest data
@@ -106,61 +132,72 @@ const Pact = ({ address }) => {
       // eslint-disable-next-line no-prototype-builtins
       if (PACT_STATES.hasOwnProperty(_pactState)) {
         // eslint-disable-next-line no-underscore-dangle
-        setListingState(PACT_STATES[_pactState]);
+        setPactState(PACT_STATES[_pactState]);
       }
     }
     setData();
-  }, [pact, signer])
+  }, [pact, signer]);
 
   // TODO show the compliance on hover
   return (
     <Grommet>
       {!loading && (
-        <Box align="center" justify="start" pad="medium">
+        <Box
+          direction="row-responsive"
+          justify="center"
+          align="center"
+          pad="medium"
+        >
           <DataChart
-            data={data}
+            data={gridLoad}
             series={[
               {
-                property: 'date',
+                property: "date",
                 render: (date) => (
                   <Box pad="xsmall" align="start">
                     <Text>
-                      {new Date(date).toLocaleDateString('en-US', {
-                        month: 'short',
-                        day: 'numeric',
+                      {new Date(date).toLocaleDateString("en-US", {
+                        month: "short",
+                        day: "numeric",
                       })}
                     </Text>
                   </Box>
                 ),
               },
-              'amount',
+              "amount",
             ]}
             chart={[
               {
-                property: 'amount',
-                type: 'area',
-                thickness: 'xsmall',
-                color: 'graph-0',
-                opacity: 'medium',
+                property: "amount",
+                type: "area",
+                thickness: "xsmall",
+                color: "graph-0",
+                opacity: "medium",
               },
               {
-                property: 'amount',
-                type: 'line',
-                thickness: 'xsmall',
+                property: "amount",
+                type: "line",
+                thickness: "xsmall",
                 round: true,
               },
-              { property: 'amount', type: 'bar', thickness: 'hair' },
+              { property: "amount", type: "bar", thickness: "hair" },
               {
-                property: 'amount',
-                type: 'point',
+                property: "amount",
+                type: "point",
                 round: true,
-                thickness: 'medium',
+                thickness: "medium",
               },
             ]}
-            axis={{ x: 'date', y: { property: 'amount', granularity: 'medium' } }}
-            guide={{ y: true }}
-            gap="medium"
+            guide={{
+              y: { granularity: "medium" },
+              x: { granularity: "medium" },
+            }}
+            alignSelf="center"
+            bounds="align"
+            gap="xsmall"
             pad="small"
+            size="large"
+            margin="small"
           />
         </Box>
       )}
@@ -170,7 +207,7 @@ const Pact = ({ address }) => {
         </Box>
       )}
     </Grommet>
-  )
-}
+  );
+};
 
 export default Pact;
